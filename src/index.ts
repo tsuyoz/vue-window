9cfa0b65aae29d32ad88c9aaea0448a3a769504f
Tsuyoshi Nakano <tsuyoz.xxx@gmail.com>
2020-09-25 22:56:06 +0900

    プラグイン形式に変更

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
-import Window from "./components/window/window.vue"
+import Vue, { PluginObject } from "vue"
+import VueWindow from "./components/window/window.vue"
+
+declare global {
+    interface Window {
+        Vue: Vue | undefined
+    }
+}
+
+interface Option {
+    componentName?: string
+}
+
+export const plugin: PluginObject<Option> = {
+    install(vue: typeof Vue, options?: Option): void {
+        const componentName = options?.componentName ?? "mc-window"
+        vue.component(componentName, VueWindow)
+    },
+}
+
+if (window.Vue) {
+    Vue.use(plugin.install)
+}
 
 export * from "./components/window/window-manager-hook"
 export * from "./components/window/window-manager-store"
 export * from "./components/window/window-store"
-export default Window
+// export default Window
+export default plugin
